bff5f142c7c545a045b967548918d68e9a7c5fed
Shiqing Wei <shiqing.wei@outlook.com>
2025-02-16 13:35:38 -0500

    backup

---
+++ eg1_inverted_pendulum/gen_sh_dreal_solve.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+
+def generate_sh_script_dreal_inclusion_solve(filename, exp_nums, dreal_precision):
+    with open(filename, "w") as file:
+        for exp_num in exp_nums:
+            command1 = f"mkdir eg1_results/{exp_num:03d}\n"
+            command2 = f"python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num {exp_num} " + \
+                f"--dreal_precision {dreal_precision:.1E} " + \
+                f"> eg1_results/{exp_num:03d}/output_dreal_inclusion_solve_{dreal_precision:.1E}.out\n"
+            file.write(command1)
+            file.write(command2)
+
+def generate_sh_script_dreal_feasibility_solve(filename, exp_nums, dreal_precision):
+    with open(filename, "w") as file:
+        for exp_num in exp_nums:
+            command1 = f"mkdir eg1_results/{exp_num:03d}\n"
+            command2 = f"python -u eg1_inverted_pendulum/dreal_feasibility_solve.py --exp_num {exp_num} " + \
+                f"--dreal_precision {dreal_precision:.1E} " + \
+                f"> eg1_results/{exp_num:03d}/output_dreal_feasibility_solve_{dreal_precision:.1E}.out\n"
+            file.write(command1)
+            file.write(command2)
+
+exp_nums = list(range(1, 13))
+
+dreal_precision = 1e-3
+
+file = os.path.join(str(Path(__file__).parent.parent), f"run_dreal_inclusion_solve.sh")
+generate_sh_script_dreal_inclusion_solve(file, exp_nums, dreal_precision)
+
+file = os.path.join(str(Path(__file__).parent.parent), f"run_dreal_feasibility_solve.sh")
+generate_sh_script_dreal_feasibility_solve(file, exp_nums, dreal_precision)
---
+++ run_dreal_inclusion_solve.sh
@@ -0,0 +1,24 @@
+mkdir eg1_results/001
+python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num 1 --dreal_precision 1.0E-03 > eg1_results/001/output_dreal_inclusion_solve_1.0E-03.out
+mkdir eg1_results/002
+python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num 2 --dreal_precision 1.0E-03 > eg1_results/002/output_dreal_inclusion_solve_1.0E-03.out
+mkdir eg1_results/003
+python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num 3 --dreal_precision 1.0E-03 > eg1_results/003/output_dreal_inclusion_solve_1.0E-03.out
+mkdir eg1_results/004
+python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num 4 --dreal_precision 1.0E-03 > eg1_results/004/output_dreal_inclusion_solve_1.0E-03.out
+mkdir eg1_results/005
+python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num 5 --dreal_precision 1.0E-03 > eg1_results/005/output_dreal_inclusion_solve_1.0E-03.out
+mkdir eg1_results/006
+python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num 6 --dreal_precision 1.0E-03 > eg1_results/006/output_dreal_inclusion_solve_1.0E-03.out
+mkdir eg1_results/007
+python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num 7 --dreal_precision 1.0E-03 > eg1_results/007/output_dreal_inclusion_solve_1.0E-03.out
+mkdir eg1_results/008
+python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num 8 --dreal_precision 1.0E-03 > eg1_results/008/output_dreal_inclusion_solve_1.0E-03.out
+mkdir eg1_results/009
+python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num 9 --dreal_precision 1.0E-03 > eg1_results/009/output_dreal_inclusion_solve_1.0E-03.out
+mkdir eg1_results/010
+python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num 10 --dreal_precision 1.0E-03 > eg1_results/010/output_dreal_inclusion_solve_1.0E-03.out
+mkdir eg1_results/011
+python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num 11 --dreal_precision 1.0E-03 > eg1_results/011/output_dreal_inclusion_solve_1.0E-03.out
+mkdir eg1_results/012
+python -u eg1_inverted_pendulum/dreal_inclusion_solve.py --exp_num 12 --dreal_precision 1.0E-03 > eg1_results/012/output_dreal_inclusion_solve_1.0E-03.out
