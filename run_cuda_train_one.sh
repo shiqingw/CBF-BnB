5a8a3bd75403b26f869c6373849da3bb5e6a24f9
Shiqing Wei <shiqing.wei@outlook.com>
2025-02-28 01:45:27 -0500

    backup eg2

--- run_cuda_train_one.sh
+++ run_cuda_train_one.sh
@@ -1,12 +1,12 @@
-mkdir eg3_results/017
-mkdir eg3_results/017
-python -u eg3_unicycle/train.py --exp_num 17 --device cuda > eg3_results/017/output.out
-mkdir eg3_results/018
-mkdir eg3_results/018
-python -u eg3_unicycle/train.py --exp_num 18 --device cuda > eg3_results/018/output.out
-mkdir eg3_results/019
-mkdir eg3_results/019
-python -u eg3_unicycle/train.py --exp_num 19 --device cuda > eg3_results/019/output.out
-mkdir eg3_results/020
-mkdir eg3_results/020
-python -u eg3_unicycle/train.py --exp_num 20 --device cuda > eg3_results/020/output.out
+mkdir eg2_results/017
+mkdir eg2_results/017
+python -u eg2_point_robot/train.py --exp_num 17 --device cuda > eg2_results/017/output.out
+mkdir eg2_results/018
+mkdir eg2_results/018
+python -u eg2_point_robot/train.py --exp_num 18 --device cuda > eg2_results/018/output.out
+mkdir eg2_results/019
+mkdir eg2_results/019
+python -u eg2_point_robot/train.py --exp_num 19 --device cuda > eg2_results/019/output.out
+mkdir eg2_results/020
+mkdir eg2_results/020
+python -u eg2_point_robot/train.py --exp_num 20 --device cuda > eg2_results/020/output.out
--- run_grid_inclusion_solve.sh
+++ run_grid_inclusion_solve.sh
@@ -1,8 +1,8 @@
-mkdir eg3_results/017
-python -u eg3_unicycle/grid_inclusion_solve_second_order.py --exp_num 17 --device cuda --precision 1.0E-03 > eg3_results/017/output_grid_inclusion_solve_cuda_1.0E-03.out
-mkdir eg3_results/018
-python -u eg3_unicycle/grid_inclusion_solve_second_order.py --exp_num 18 --device cuda --precision 1.0E-03 > eg3_results/018/output_grid_inclusion_solve_cuda_1.0E-03.out
-mkdir eg3_results/019
-python -u eg3_unicycle/grid_inclusion_solve_second_order.py --exp_num 19 --device cuda --precision 1.0E-03 > eg3_results/019/output_grid_inclusion_solve_cuda_1.0E-03.out
-mkdir eg3_results/020
-python -u eg3_unicycle/grid_inclusion_solve_second_order.py --exp_num 20 --device cuda --precision 1.0E-03 > eg3_results/020/output_grid_inclusion_solve_cuda_1.0E-03.out
+mkdir eg2_results/017
+python -u eg2_point_robot/grid_inclusion_solve_second_order.py --exp_num 17 --device cuda --precision 1.0E-03 > eg2_results/017/output_grid_inclusion_solve_cuda_1.0E-03.out
+mkdir eg2_results/018
+python -u eg2_point_robot/grid_inclusion_solve_second_order.py --exp_num 18 --device cuda --precision 1.0E-03 > eg2_results/018/output_grid_inclusion_solve_cuda_1.0E-03.out
+mkdir eg2_results/019
+python -u eg2_point_robot/grid_inclusion_solve_second_order.py --exp_num 19 --device cuda --precision 1.0E-03 > eg2_results/019/output_grid_inclusion_solve_cuda_1.0E-03.out
+mkdir eg2_results/020
+python -u eg2_point_robot/grid_inclusion_solve_second_order.py --exp_num 20 --device cuda --precision 1.0E-03 > eg2_results/020/output_grid_inclusion_solve_cuda_1.0E-03.out
